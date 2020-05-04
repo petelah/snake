e7972728874ddcf4c796debe7b52423868f18635
petelah <bboy.pete@gmail.com>
2020-05-04 23:44:17 +1000

    adding highscores function

--- snake.py
+++ snake.py
@@ -1,10 +1,13 @@
 import pygame
 import json
+from datetime import datetime
 from os import getcwd, path
 from random import choice
 
 from player_two import snake
 from dot import dot
+from pygame_textinput import *
+from utils import load_hs, save_hs
 
 # Init pygame
 pygame.init()
@@ -13,9 +16,14 @@ pygame.init()
 WIDTH = 500
 HEIGHT = 550
 font = pygame.font.SysFont('helvetica', 30, True)
+hsfont = pygame.font.SysFont('helvetica', 45, True)
 
 # Game clock
 clock = pygame.time.Clock()
+
+# Create TextInput-object
+textinput = TextInput(text_color=(0,255,0), cursor_color=(0,255,0),
+                      font_family="helvetica", font_size=45, max_string_length=5)
 
 # Create screen
 window = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -27,6 +35,8 @@ snakey = snake(300, 410, 10, 10)
 apple = dot(250, 250)
 score = 0
 speed = 1
+
+highscores = load_hs()
 
 def main_menu():
 
@@ -72,23 +82,64 @@ def game_over(score):
     barXpos = 50
     barYpos = 230
     barLength = 115
+    run = True
+    hs = {'PETE1': [60, '27.04.2020'], 'Marcy': [700, '20.04.2020']}
+    show_text = True
 
-    while True:
+    while run:
         window.fill((0, 0, 0))
         pygame.draw.rect(window, (0, 255, 0), (4, 55, 491, 490), 10)
-        startText = font.render('START', 1, (0, 255, 0))
-        window.blit(startText, (50, 200))
-        exitText = font.render('EXIT', 1, (0, 255, 0))
-        window.blit(exitText, (350, 200))
-        pygame.draw.rect(window, (0, 255, 0), (barXpos, barYpos, barLength, 5))
+        goText = hsfont.render('HIGHSCORES', 1, (0, 255, 0))
+        window.blit(goText, (75, 65))
+        hsname = font.render('NAME', 1, (0, 255, 0))
+        hslvl = font.render('LVL', 1, (0, 255, 0))
+        hsdate = font.render('DATE', 1, (0, 255, 0))
+        window.blit(hsname, (25, 120))
+        window.blit(hsdate, (175, 120))
+        window.blit(hslvl, (300, 120))
+        error_border = (255,0,0)
+        norm_border = (0,255,0)
+        pygame.draw.rect(window, (255,0,0), (25, 475, 450, 50),5)
+        events = pygame.event.get()
 
+        if show_text:
+            # Feed it with events every frame
+            textinput.update(events)
+            # Blit its surface onto the screen
+            window.blit(textinput.get_surface(), (30, 480))
+
+        i = 0
+        for k,v in highscores.items():
+            name = font.render(f'{k}', 1, (0, 255, 0))
+            nscore = font.render(f'{v[0]}', 1, (0, 255, 0))
+            date = font.render(f'{v[1]}', 1, (0, 255, 0))
+            window.blit(name, (25, 150+i))
+            window.blit(nscore, (175, 150 + i))
+            window.blit(date, (300, 150 + i))
+            i += 25
+
+        #pygame.draw.rect(window, (0, 255, 0), (barXpos, barYpos, barLength, 5))
+        clock.tick(60)
+        pygame.display.update()
+
+        # Look for quit types
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            return False
+        if keys[pygame.K_RETURN]:
+            highscores[textinput.get_text()] = [f"{score}", f"{datetime.now().strftime('%d.%m.%Y')}"]
+            save_hs(highscores)
+            show_text = False
 
 def main_game():
     run = True
     global score
     global speed
     tickspeed = 15
-    while run == True:
+    while run:
         window.fill((0, 0, 0))
         scoreText = font.render('SCORE: ' + str(score), 1, (0, 255, 0))
         speedText = font.render('LEVEL: ' + str(speed), 1, (0, 255, 0))
@@ -151,6 +202,8 @@ def main_game():
             #print('hit')
             score = 0
             snakey.length = [[300, 410], []]
+            # goto gameover menu
+            game_over(score)
             # return to main menu
             return False
 
@@ -168,6 +221,9 @@ def main_game():
         # Check to see if snake hit itself
         if len(snakey.length) > 3:
             if snakey.length[0] in snakey.length[2:]:
+                # goto gameover menu
+                game_over(score)
+                # return to main menu
                 return False
 
 
---
+++ utils.py
@@ -0,0 +1,33 @@
+import json
+from os import getcwd, path
+from pathlib import Path
+
+def load_hs():
+    """
+    Function loads highscore json file.
+    If none found it creates one so scores can be saved.
+    :return:
+    """
+    # check if there is already a json file to load
+    cwd = path.join(getcwd(), 'highscore.json')
+    # if not, create one and return it
+    if not path.isfile(cwd):
+        Path(cwd).touch()
+        return False
+    # if there is, return it
+    else:
+        with open(cwd) as json_file:
+            data = json.load(json_file)
+        return data
+
+def save_hs(hs):
+    """
+    Saves last played highscore to variable
+    :return:
+    """
+    cwd = path.join(getcwd(), 'highscore.json')
+    with open(cwd, 'w') as outfile:
+        json.dump(hs, outfile)
+
+
+
